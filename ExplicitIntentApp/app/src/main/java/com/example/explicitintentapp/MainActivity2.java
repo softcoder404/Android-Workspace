46df56eeb644b3ed7379e668f52cda2b40db2f7a
softcoder404 <daseeker9@gmail.com>
2020-07-31 13:00:36 +0100

    implicit intent logic added

--- ExplicitIntentApp/app/src/main/java/com/example/explicitintentapp/MainActivity2.java
+++ ExplicitIntentApp/app/src/main/java/com/example/explicitintentapp/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.explicitintentapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
     TextView tvRes;
-    Button btnCancel;
+    Button btnCancel, btnCall, btnCallFriend, btnWeb, btnMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +20,52 @@ public class MainActivity2 extends AppCompatActivity {
 
         tvRes = findViewById(R.id.tvRes);
         btnCancel = findViewById(R.id.btnCancel2);
-        Intent intent = getIntent();
-        String name = intent.getStringExtra("fullName");
-        tvRes.setText("Hi "+name);
+        btnCall = findViewById(R.id.btnCall);
+        btnCallFriend = findViewById(R.id.btnCallFriend);
+        btnWeb = findViewById(R.id.btnWeb);
+        btnMap = findViewById(R.id.btnMap);
+
+
+        Intent getIntent = getIntent();
+        String name = "Hey ! " + getIntent.getStringExtra("fullName") + " Welcome to implicit intent";
+        tvRes.setText(name);
 
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             MainActivity2.this.finish();
+            }
+        });
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:"));
+                startActivity(intent);
+            }
+        });
+
+        btnCallFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:07014235169"));
+                startActivity(intent);
+            }
+        });
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q= Central Mosque Ilorin Kwara State"));
+                startActivity(intent);
+            }
+        });
+
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://github.com/softcoder404"));
+                startActivity(intent);
             }
         });
     }
