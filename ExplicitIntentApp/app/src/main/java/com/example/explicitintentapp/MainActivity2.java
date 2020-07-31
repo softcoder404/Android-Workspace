923477ee167523af186e4e0a4afc1bb87e15ff1a
softcoder404 <daseeker9@gmail.com>
2020-07-31 09:02:45 +0100

    on return null data

--- ExplicitIntentApp/app/src/main/java/com/example/explicitintentapp/MainActivity2.java
+++ ExplicitIntentApp/app/src/main/java/com/example/explicitintentapp/MainActivity2.java
@@ -2,6 +2,7 @@ package com.example.explicitintentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,13 +19,14 @@ public class MainActivity2 extends AppCompatActivity {
 
         tvRes = findViewById(R.id.tvRes);
         btnCancel = findViewById(R.id.btnCancel2);
-        String name = getIntent().getStringExtra("fullName");
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("fullName");
         tvRes.setText("Hi "+name);
 
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+            MainActivity2.this.finish();
             }
         });
     }
