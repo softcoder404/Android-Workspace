db60a7cba7a79d6b2669edb1a026079f23ff0de9
softcoder404 <daseeker9@gmail.com>
2020-08-02 10:12:37 +0100

    notify adapter change

--- RecyclerAndCardView/app/src/main/java/com/example/recyclerandcardview/MainActivity.java
+++ RecyclerAndCardView/app/src/main/java/com/example/recyclerandcardview/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -14,11 +16,13 @@ public class MainActivity extends AppCompatActivity implements PersonAdapter.Cli
     RecyclerView.Adapter recyclerAdapter;
     RecyclerView.LayoutManager layoutManager;
     ArrayList<PersonModel> person;
+    Button btnAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnAdd = findViewById(R.id.btnAdd);
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
@@ -33,26 +37,15 @@ public class MainActivity extends AppCompatActivity implements PersonAdapter.Cli
         person.add(new PersonModel("Angela","Yu","plane"));
         person.add(new PersonModel("Traversy","Scott","bus"));
         person.add(new PersonModel("Allen","John","plane"));
-        person.add(new PersonModel("AbdulHameed","Quadri","plane"));
-        person.add(new PersonModel("Olamilekan","Jimoh","plane"));
-        person.add(new PersonModel("Adam","AbdulHakeem","plane"));
-        person.add(new PersonModel("Stark","Azeez","plane"));
-        person.add(new PersonModel("Tom","Cruise","bus"));
-        person.add(new PersonModel("Jeremy","Blum","bus"));
-        person.add(new PersonModel("Angela","Yu","plane"));
-        person.add(new PersonModel("Traversy","Scott","bus"));
-        person.add(new PersonModel("Allen","John","plane"));
-        person.add(new PersonModel("AbdulHameed","Quadri","plane"));
-        person.add(new PersonModel("Olamilekan","Jimoh","plane"));
-        person.add(new PersonModel("Adam","AbdulHakeem","plane"));
-        person.add(new PersonModel("Stark","Azeez","plane"));
-        person.add(new PersonModel("Tom","Cruise","bus"));
-        person.add(new PersonModel("Jeremy","Blum","bus"));
-        person.add(new PersonModel("Angela","Yu","plane"));
-        person.add(new PersonModel("Traversy","Scott","bus"));
-        person.add(new PersonModel("Allen","John","plane"));
 
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                person.add(new PersonModel("John","Doe","Bus"));
+            }
+        });
         recyclerAdapter = new PersonAdapter(this,person);
+        recyclerAdapter.notifyDataSetChanged();
         recyclerView.setAdapter(recyclerAdapter);
     }
 
