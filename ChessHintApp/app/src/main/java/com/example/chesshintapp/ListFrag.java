83366be0d9e63b32fc22e4849bbf6135baef4f00
softcoder404 <daseeker9@gmail.com>
2020-08-13 23:04:30 +0100

    setListAdapter

--- ChessHintApp/app/src/main/java/com/example/chesshintapp/ListFrag.java
+++ ChessHintApp/app/src/main/java/com/example/chesshintapp/ListFrag.java
@@ -1,13 +1,20 @@
 package com.example.chesshintapp;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListFrag extends ListFragment {
 
@@ -18,9 +25,27 @@ public class ListFrag extends ListFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ArrayList<String> data = new ArrayList<String>();
+        data.add("1. This is item 1");
+        data.add("2. This is item 2");
+        data.add("3. This is item 3");
+        data.add("4. This is item 4");
+
+        //to put the list items inside our list fragment will need an adapter
+        //setListAdapter(new ArrayAdapter<type>(context,layout_file,data))
+        setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, data ));
+    }
+
+    @Override
+    public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
+
     }
 }
