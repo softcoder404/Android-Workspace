51de6d4224d27fa64213d96e8d26b312fa532098
softcoder404 <daseeker9@gmail.com>
2020-08-14 11:06:27 +0100

    file modified

--- ChessHintApp/app/src/main/java/com/example/chesshintapp/MainActivity.java
+++ ChessHintApp/app/src/main/java/com/example/chesshintapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.chesshintapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.View;
@@ -18,11 +19,37 @@ public class MainActivity extends AppCompatActivity implements ListFrag.ItemSele
         tvDescription = findViewById(R.id.tvDescription);
         tvDescription.setVisibility(View.GONE);
         descriptions = getResources().getStringArray(R.array.descriptions);
+        //if the phone is in portrait
+        if(findViewById(R.id.layout_portrait) != null){
+            FragmentManager fragmentManager = this.getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .hide(fragmentManager.findFragmentById(R.id.detailFrag))
+                    .show(fragmentManager.findFragmentById(R.id.listFrag))
+                    .commit();
+        }
+        //if the phone is in landscape
+
+        if(findViewById(R.id.layout_landscape) != null){
+            FragmentManager fragmentManager = this.getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .show(fragmentManager.findFragmentById(R.id.detailFrag))
+                    .show(fragmentManager.findFragmentById(R.id.listFrag))
+                    .commit();
+        }
     }
 
     @Override
     public void onItemSelected(int index) {
         tvDescription.setText(descriptions[index]);
         tvDescription.setVisibility(View.VISIBLE);
+        //if the phone is in portrait
+        if(findViewById(R.id.layout_portrait) != null){
+            FragmentManager fragmentManager = this.getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .show(fragmentManager.findFragmentById(R.id.detailFrag))
+                    .hide(fragmentManager.findFragmentById(R.id.listFrag))
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 }
